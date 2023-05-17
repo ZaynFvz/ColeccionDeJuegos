86de46530dcd88c0662341674d5cf6b178aea909
ZaynFvz <gonzalo.vargas@tecsup.edu.pe>
2023-05-17 09:43:51 -0500

    Initial Commit

---
+++ VargasColecciondeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VargasColecciondeJuegos
+//
+//  Created by Gonzalo Vargas on 17/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
